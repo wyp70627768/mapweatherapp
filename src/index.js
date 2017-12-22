38de2afbbd09a56f39d231e36658d4fdf5096707
Yingpeng Wang <yp.wangt@gmail.com>
2017-12-22 13:17:05 -0600

    clean unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+import Home from './components/Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const $ = window.$;
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {city: null};
+      this.getCityFromSearchInput = this.getCityFromSearchInput.bind(this);
+  }
+  getCityFromSearchInput(city) {
+    function edit(val) {
+        let str = 
+        $.trim(val)
+            .toLowerCase()
+            .replace(/[0-9]/g, '')
+            .replace(/^[a-z\d,]*\-?[a-z\d,]*$ /g, '')
+            .replace(/\s+/g, ' ');
+
+        console.log(str);
+        return (
+            str === null || str === ''
+                ? 'not valid' : str
+        )
+    }
+    const editedValue = edit(city);
+    
+            if (editedValue === 'not valid') return;
+            
+            this.setState({ city: editedValue })  
+  }
+  render() {
+    const isValidCity = this.state.city !== null;
+
+    return (
+        isValidCity ? 
+            <Fragment>
+                <Home onClick={this.getCityFromSearchInput}/>
+                <FetchData city={this.state.city}/>
+            </Fragment> 
+            :
+            <Home onClick={this.getCityFromSearchInput}/>
+    )
+  }
+}
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
